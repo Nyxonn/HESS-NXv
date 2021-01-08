881a00ff3331778abb2eae5e5cf00b1bb941d6e7
Nyxonn <45857590+Nyxonn@users.noreply.github.com>
2021-01-08 22:10:48 +0100

    HESS 1.0 NXv

---
+++ harvester/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace harvester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.xuiSuperButton1 = new XanderUI.XUISuperButton();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.BackColor = System.Drawing.Color.Transparent;
+            this.metroButton1.FontSize = MetroFramework.MetroButtonSize.Medium;
+            this.metroButton1.ForeColor = System.Drawing.Color.Fuchsia;
+            this.metroButton1.Location = new System.Drawing.Point(6, 6);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(233, 80);
+            this.metroButton1.TabIndex = 0;
+            this.metroButton1.Text = "Choose folder";
+            this.metroButton1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.Black;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.9F);
+            this.richTextBox1.ForeColor = System.Drawing.Color.Fuchsia;
+            this.richTextBox1.Location = new System.Drawing.Point(6, 92);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(756, 287);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.FontSize = MetroFramework.MetroButtonSize.Medium;
+            this.metroButton2.Location = new System.Drawing.Point(529, 6);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(233, 80);
+            this.metroButton2.TabIndex = 3;
+            this.metroButton2.Text = "Harvest";
+            this.metroButton2.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton2.UseSelectable = true;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(6, 393);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(233, 19);
+            this.metroLabel1.TabIndex = 4;
+            this.metroLabel1.Text = "Original tool made by yunglean_#4171";
+            // 
+            // xuiSuperButton1
+            // 
+            this.xuiSuperButton1.BackgroundColor = System.Drawing.Color.Transparent;
+            this.xuiSuperButton1.ButtonImage = ((System.Drawing.Image)(resources.GetObject("xuiSuperButton1.ButtonImage")));
+            this.xuiSuperButton1.ButtonSmoothing = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            this.xuiSuperButton1.ButtonStyle = XanderUI.XUISuperButton.Style.RoundedEdges;
+            this.xuiSuperButton1.ButtonText = "";
+            this.xuiSuperButton1.CornerRadius = 5;
+            this.xuiSuperButton1.Horizontal_Alignment = System.Drawing.StringAlignment.Center;
+            this.xuiSuperButton1.HoverBackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.xuiSuperButton1.HoverTextColor = System.Drawing.Color.White;
+            this.xuiSuperButton1.ImagePosition = XanderUI.XUISuperButton.imgPosition.Left;
+            this.xuiSuperButton1.Location = new System.Drawing.Point(728, 381);
+            this.xuiSuperButton1.Name = "xuiSuperButton1";
+            this.xuiSuperButton1.SelectedBackColor = System.Drawing.Color.Gray;
+            this.xuiSuperButton1.SelectedTextColor = System.Drawing.Color.White;
+            this.xuiSuperButton1.Size = new System.Drawing.Size(36, 34);
+            this.xuiSuperButton1.SuperSelected = false;
+            this.xuiSuperButton1.TabIndex = 5;
+            this.xuiSuperButton1.TextColor = System.Drawing.Color.White;
+            this.xuiSuperButton1.Vertical_Alignment = System.Drawing.StringAlignment.Center;
+            this.xuiSuperButton1.Click += new System.EventHandler(this.xuiSuperButton1_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(776, 441);
+            this.tabControl1.TabIndex = 7;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.metroLabel3);
+            this.tabPage2.Controls.Add(this.richTextBox3);
+            this.tabPage2.Controls.Add(this.metroLabel2);
+            this.tabPage2.Controls.Add(this.richTextBox1);
+            this.tabPage2.Controls.Add(this.metroLabel1);
+            this.tabPage2.Controls.Add(this.metroButton1);
+            this.tabPage2.Controls.Add(this.xuiSuperButton1);
+            this.tabPage2.Controls.Add(this.metroButton2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(768, 415);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Main page";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.Location = new System.Drawing.Point(288, 22);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(125, 19);
+            this.metroLabel3.TabIndex = 8;
+            this.metroLabel3.Text = "String to search for:";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.BackColor = System.Drawing.Color.Black;
+            this.richTextBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.9F);
+            this.richTextBox3.ForeColor = System.Drawing.Color.Fuchsia;
+            this.richTextBox3.Location = new System.Drawing.Point(292, 44);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(193, 27);
+            this.richTextBox3.TabIndex = 7;
+            this.richTextBox3.Text = "";
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(245, 393);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(227, 19);
+            this.metroLabel2.TabIndex = 6;
+            this.metroLabel2.Text = "Upgraded tool made by Nyxon#4418";
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.listBox1);
+            this.tabPage1.Controls.Add(this.richTextBox2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(768, 415);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Results";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.Black;
+            this.listBox1.ForeColor = System.Drawing.Color.Fuchsia;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(6, 312);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(756, 95);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.Color.Black;
+            this.richTextBox2.ForeColor = System.Drawing.Color.Fuchsia;
+            this.richTextBox2.Location = new System.Drawing.Point(6, 6);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(756, 300);
+            this.richTextBox2.TabIndex = 0;
+            this.richTextBox2.Text = "";
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(791, 462);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "HΣSS NXv";
+            this.Load += new System.EventHandler(this.main_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private XanderUI.XUISuperButton xuiSuperButton1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+    }
+}
+
---
+++ harvester/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System.Windows.Forms;
+using System.Drawing;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace harvester
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public bool chosen = false;
+        public string paczka;
+        public string filetoload;
+        public string searchingfor;
+        public int ilosc = 0;
+        public int ilosctrg = 0;
+
+        public void Log(string text)
+        {
+            richTextBox1.Invoke(new MethodInvoker(delegate () { richTextBox1.AppendText(text + "\r\n"); richTextBox1.ScrollToCaret(); }));
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.InitialDirectory = "C:\\";
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                richTextBox1.Text = "";
+                Log("➤ Folder path has been set to: " + dialog.FileName);
+                chosen = true;
+                paczka = dialog.FileName;
+            }
+        }
+
+        private void xuiSuperButton1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/Nyxonn");
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            ilosctrg = 0;
+            ilosc = 0;
+            if (chosen == false)
+            {
+                richTextBox1.Text = "";
+                Log("⚠️ Select folder with .lua file before extraction!");
+            }
+            else
+            {
+                listBox1.Items.Clear();
+                richTextBox2.Text = "";
+                searchingfor = richTextBox3.Text;
+
+                DirectoryInfo dinfo = new DirectoryInfo(paczka);
+
+                Regex FileRegex = new Regex(@"\*\.\w\w\w");
+                FileInfo[] Files;
+                foreach (Match match in FileRegex.Matches("*.lua"))
+                {
+                    Files = dinfo.GetFiles(match.Value);
+
+                    tabControl1.SelectTab(1);
+
+
+                    foreach (FileInfo file in Files)
+                    {
+                        listBox1.Items.Add(file.Name);
+                        filetoload = paczka + "\\" + file.Name;
+                        string pliczekNazwa = file.Name;
+                        foreach (var line in File.ReadAllLines(filetoload))
+                        {
+                            if (line.Contains("--"))
+                            {
+
+                            }
+                            else
+                            {
+                                if (line.Contains(searchingfor.ToString()))
+                                {
+                                    richTextBox2.AppendText(pliczekNazwa + ": " + line.TrimStart() + "\n\n");
+                                    richTextBox2.ScrollToCaret();
+                                    ilosctrg = ilosctrg + 1;
+                                }
+                            }
+                            ilosc = ilosc + 1;
+                        }
+                      
+
+
+
+                    }
+                    Log("★ Readed " + ilosc + " lines and found " + ilosctrg + " potential strings!");
+                }
+            }
+        }
+
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ilosctrg = 0;
+            ilosc = 0;
+            filetoload = paczka + @"\" + listBox1.SelectedItem;
+            string pliczekNazwa = listBox1.SelectedItem.ToString();
+            string pliczek = File.ReadAllText(filetoload);
+            searchingfor = richTextBox3.Text;
+            if (pliczek.Contains(searchingfor))
+            {
+                richTextBox2.AppendText("\n\n");
+                foreach (var line in File.ReadAllLines(filetoload))
+                {
+                    if (line.Contains("--")) { 
+                        
+                    } else {
+                        if (line.Contains(searchingfor))
+                        {
+                            richTextBox2.AppendText(pliczekNazwa + ": " + line.TrimStart() + "\n\n");
+                            richTextBox2.ScrollToCaret();
+                            ilosctrg = ilosctrg + 1;
+                        }
+                    }
+                    ilosc = ilosc + 1;
+                }
+                Log("★ " + pliczekNazwa + ": Readed " + ilosc + " lines and found " + ilosctrg + " potential strings!");
+            } else
+            {
+                richTextBox2.AppendText("\n" + pliczekNazwa + ": " + "No triggers were found\n");
+                richTextBox2.ScrollToCaret();
+                Log("✫ " + pliczekNazwa + ": Readed " + ilosc + " lines and found none potential strings!");
+            }
+        }
+
+        private void main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTextBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
